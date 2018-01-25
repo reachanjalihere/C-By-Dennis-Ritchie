84d2a68e9e982ed6e615eb9a674e4c404cfa7307
Abhimanyu Rawat <anjalisorout24@gmail.com>
2018-01-25 13:25:47 -0500

    Debut commit

---
+++ src/chapter_1/exercise_1_1.c
@@ -0,0 +1,12 @@
+/* Run the "hello, world" program on your system. Experiment with leaving
+   out parts of the program, to see what error message you get. */
+
+
+#include<stdio.h>
+
+int main()
+{
+	printf("hello, world \n");  // this statement prints the hello, world
+
+	return 0;
+}
